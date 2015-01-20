402735bdecca32717d7328fc4a65e17cafda4e15
James Creel <jcreel@library.tamu.edu>
2015-01-20 09:49:21 -0600

    Removed debugging alert from license step javascript

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/license-step.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/license-step.js
@@ -1,6 +1,5 @@
 jQuery(document).ready(function(){
 	// License step foo
-	alert("foo.");
    	
    	// Hide everything until the user makes a choice
    	jQuery(document).concealLicenseText();
@@ -13,6 +12,7 @@ jQuery(document).ready(function(){
    	    jQuery("#aspect_submission_StepTransformer_div_submit-license-standard-text").show();
    	    jQuery("#aspect_submission_StepTransformer_div_submit-license-inner #aspect_submission_StepTransformer_list_submit-review").show();
    	});
+   	
    	
    	jQuery("input[name='license-selector'][value='proxy']").click( function() {
    	    jQuery(document).concealLicenseText();
