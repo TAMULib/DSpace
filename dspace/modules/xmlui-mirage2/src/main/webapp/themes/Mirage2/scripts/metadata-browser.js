bbf38fea1ab190dbd42c8cb45d0c276b038ea875
jsavell <jsavell@library.tamu.edu>
2015-05-12 13:21:18 -0500

    update metadata browser js to use Mirage2 glyphicons

--- dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/metadata-browser.js
+++ dspace/modules/xmlui-mirage2/src/main/webapp/themes/Mirage2/scripts/metadata-browser.js
@@ -4,15 +4,18 @@ jQuery(document).ready(function() {
    // Add toggle controls to all elements which have children.
    jQuery("#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list li").each(function() {
       if (jQuery(this).next().has('ul').length > 0) {
-         jQuery(this).prepend("<a class=\"toggle\" href=\"javascript: void(0);\">[+]</a> ");
+         jQuery(this).prepend("<a class=\"toggle btn btn-default btn-sm toggler collapsed\" href=\"javascript: void(0);\">"+
+                                 "<i aria-hidden=\"true\" class=\"glyphicon glyphicon-plus closed-icon\"></i>"+
+                              "</a> ");
       }
    });
    
    // Toggle the state of a control, showing the children and switching the display character used.
    jQuery("#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list .toggle").click(function() {
-      if (jQuery(this).text().indexOf('[-]') > -1) {
+      $toggler = jQuery(this).children("i.glyphicon");
+      if ($toggler.hasClass("glyphicon-minus")) {
          // Collapse Children
-         jQuery(this).text('[+]');
+         $toggler.removeClass("glyphicon-minus open-icon").addClass("glyphicon-plus closed-icon");
          
          // Variable speed based upon how many children.
          count = jQuery(this).parent().next().has('li ul li').length;
@@ -21,8 +24,8 @@ jQuery(document).ready(function() {
 
       } else {
          // Expand Children
-         jQuery(this).text('[-]');
-         
+         $toggler.removeClass("glyphicon-plus closed-icon").addClass("glyphicon-minus open-icon");
+       
          count = jQuery(this).parent().next().has('li ul li').length;
          jQuery(this).parent().next().has('li').slideDown(count * 200 + 200);
       }
@@ -36,7 +39,7 @@ jQuery(document).ready(function() {
    // If there is only one top level element, auto expand it.
    if (jQuery("ul#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list.root-list").children().length <= 2) {
       jQuery("ul#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list.root-list").children().last().show();
-      jQuery("ul#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list.root-list a.toggle").first().text("[-]");
+      jQuery("ul#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list.root-list a.toggle").first().children("i.glyphicon").removeClass("glyphicon-plus closed-icon").addClass("glyphicon-minus open-icon");
    }
    
    // Add the expand all button
@@ -47,7 +50,7 @@ jQuery(document).ready(function() {
          jQuery("#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list a.toggle").each(function () {
             count = jQuery(this).parent().next().has('li ul li').length;
             jQuery(this).parent().next().has('li').slideDown(count * 200 + 200);
-            jQuery(this).text('[-]');
+            jQuery(this).children("i.glyphicon").removeClass("glyphicon-plus closed-icon").addClass("glyphicon-minus open-icon");
          });
          jQuery(this).text('Collapse All');
          
@@ -56,11 +59,10 @@ jQuery(document).ready(function() {
          jQuery("#edu_tamu_metadatatreebrowser_BrowseOverview_list_overview-list a.toggle").each(function () {
             count = jQuery(this).parent().next().has('li ul li').length;
             jQuery(this).parent().next().has('li').slideUp(count * 200 + 200);
-            jQuery(this).text('[+]');
+            jQuery(this).children("i.glyphicon").removeClass("glyphicon-minus open-icon").addClass("glyphicon-plus closed-icon");
          });
          jQuery(this).text('Expand All');
       }
       
    });
-   
 });
