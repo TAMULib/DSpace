6adf74dc1497f610e04cf24c3d35e76f975c67ce
jsavell <jsavell@library.tamu.edu>
2015-06-11 14:52:50 -0500

    configure curation tasks and add java

---
+++ dspace/modules/xmlui/src/main/java/edu/tamu/dspace/curate/DeleteThumbnails.java
@@ -0,0 +1,66 @@
+package edu.tamu.dspace.curate;
+
+import java.io.IOException;
+import java.sql.SQLException;
+
+import org.dspace.authorize.AuthorizeException;
+import org.dspace.content.Bitstream;
+import org.dspace.content.Bundle;
+import org.dspace.content.DSpaceObject;
+import org.dspace.content.Item;
+import org.dspace.core.Constants;
+import org.dspace.curate.AbstractCurationTask;
+import org.dspace.curate.Curator;
+
+
+public class DeleteThumbnails extends AbstractCurationTask {
+    
+    private int result = Curator.CURATE_SUCCESS;
+    private StringBuilder sb = new StringBuilder();
+    
+    @Override 
+    public void init(Curator curator, String taskId) throws IOException
+    {
+        super.init(curator, taskId);
+        sb = new StringBuilder();
+    }
+
+    @Override
+    public int perform(DSpaceObject dso) throws IOException 
+    {
+	    if (dso.getType() == Constants.SITE) {    	
+	    	sb.append("Cannot perform this task at site level.");
+	    	this.setResult(sb.toString());
+	    	return Curator.CURATE_FAIL;    	
+	    } else {
+	    	distribute(dso);
+	    	this.setResult(sb.toString());	
+	    	return result;
+	    }	
+    }
+    
+    @Override
+    protected void performItem(Item item) throws SQLException, IOException
+    {
+        try {
+            int count=0;
+	    for (Bundle bundle : item.getBundles("THUMBNAIL")) {
+		for (Bitstream bitstream : bundle.getBitstreams()) {
+		    bundle.removeBitstream(bitstream);
+		    count++;
+		}
+		item.removeBundle(bundle);
+	    }
+	    item.update();
+	    sb.append(item.getHandle() + ": " + count + " images deleted. \n");	    
+	} catch (AuthorizeException e) {
+	    result = Curator.CURATE_ERROR;
+	    sb.append("Authorization failure on item: " + item.getHandle() + "\nAborting...");
+	}
+    }
+    
+    
+    
+    
+
+}
