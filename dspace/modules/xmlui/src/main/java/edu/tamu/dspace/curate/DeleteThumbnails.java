1e1bfb7a4619f88d9fc1adb54669974e81a7c8b2
William Welling <wwelling@library.tamu.edu>
2016-12-09 09:11:40 -0600

    Updated delete thumbnails task.

--- dspace/modules/xmlui/src/main/java/edu/tamu/dspace/curate/DeleteThumbnails.java
+++ dspace/modules/xmlui/src/main/java/edu/tamu/dspace/curate/DeleteThumbnails.java
@@ -12,55 +12,65 @@ import org.dspace.core.Constants;
 import org.dspace.curate.AbstractCurationTask;
 import org.dspace.curate.Curator;
 
-
 public class DeleteThumbnails extends AbstractCurationTask {
-    
-    private int result = Curator.CURATE_SUCCESS;
-    private StringBuilder sb = new StringBuilder();
-    
-    @Override 
-    public void init(Curator curator, String taskId) throws IOException
-    {
+
+    private static int result;
+
+    private static StringBuilder sb;
+
+    @Override
+    public void init(Curator curator, String taskId) throws IOException {
         super.init(curator, taskId);
+        result = Curator.CURATE_SUCCESS;
         sb = new StringBuilder();
     }
 
     @Override
-    public int perform(DSpaceObject dso) throws IOException 
-    {
-	    if (dso.getType() == Constants.SITE) {    	
-	    	sb.append("Cannot perform this task at site level.");
-	    	this.setResult(sb.toString());
-	    	return Curator.CURATE_FAIL;    	
-	    } else {
-	    	distribute(dso);
-	    	this.setResult(sb.toString());	
-	    	return result;
-	    }	
+    public int perform(DSpaceObject dso) throws IOException {
+        switch (dso.getType()) {
+        case Constants.SITE:
+            sb.append("Cannot perform this task at site level.");
+            setResult(sb.toString());
+            result = Curator.CURATE_FAIL;
+            break;
+        case Constants.COMMUNITY:
+            sb.append("Deleting thumbnails in community " + dso.getHandle() + "\n");
+            setResult(sb.toString());
+            break;
+        case Constants.COLLECTION:
+            sb.append("Deleting thumbnails in collection " + dso.getHandle() + "\n");
+            setResult(sb.toString());
+            break;
+        case Constants.ITEM:
+            Item item = (Item) dso;
+            try {
+                int count = 0;
+                for (Bundle bundle : item.getBundles("THUMBNAIL")) {
+                    for (Bitstream bitstream : bundle.getBitstreams()) {
+                        bundle.removeBitstream(bitstream);
+                        count++;
+                    }
+                    item.removeBundle(bundle);
+                }
+                item.update();
+                sb.append(item.getHandle() + ": " + count + " images deleted.\n");
+                setResult(sb.toString());
+            } catch (SQLException e) {
+                sb.append("Failed to persist change on item: " + item.getHandle() + "\nAborting...");
+                setResult(sb.toString());
+                result = Curator.CURATE_ERROR;
+            } catch (AuthorizeException e) {
+                sb.append("Authorization failure on item: " + item.getHandle() + "\nAborting...");
+                setResult(sb.toString());
+                result = Curator.CURATE_ERROR;
+            }
+            break;   
+        
+        default:
+            
+            break;
+        }
+        return result;
     }
-    
-    @Override
-    protected void performItem(Item item) throws SQLException, IOException
-    {
-        try {
-            int count=0;
-	    for (Bundle bundle : item.getBundles("THUMBNAIL")) {
-		for (Bitstream bitstream : bundle.getBitstreams()) {
-		    bundle.removeBitstream(bitstream);
-		    count++;
-		}
-		item.removeBundle(bundle);
-	    }
-	    item.update();
-	    sb.append(item.getHandle() + ": " + count + " images deleted. \n");	    
-	} catch (AuthorizeException e) {
-	    result = Curator.CURATE_ERROR;
-	    sb.append("Authorization failure on item: " + item.getHandle() + "\nAborting...");
-	}
-    }
-    
-    
-    
-    
 
 }
