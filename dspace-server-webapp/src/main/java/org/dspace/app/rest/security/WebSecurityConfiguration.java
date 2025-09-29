9c78e567c2ecab469b044a7cedbdd06d22c99c1f
tamu-sad-iii <wwelling@tamu.edu>
2025-09-29 15:49:50 -0500

    Reorder explicit folders as defined to insert before

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/WebSecurityConfiguration.java
@@ -150,6 +150,13 @@ public class WebSecurityConfiguration {
             // (e.g. anonymous users may be added to special DSpace groups if they are in a given IP range)
             .addFilterBefore(new AnonymousAdditionalAuthorizationFilter(authenticationManager, authenticationService),
                              StatelessAuthenticationFilter.class)
+
+            // Add a custom Token based authentication filter based on the token previously given to the client
+            // before each URL
+            .addFilterBefore(new StatelessAuthenticationFilter(authenticationManager, restAuthenticationService,
+                                                               ePersonRestAuthenticationProvider, requestService),
+                             StatelessLoginFilter.class)
+
             // Add a filter before our login endpoints to do the authentication based on the data in the HTTP request.
             // This login endpoint only responds to POST as it is used for PasswordAuthentication
             .addFilterBefore(new StatelessLoginFilter("/api/authn/login", HttpMethod.POST.name(),
@@ -176,12 +183,7 @@ public class WebSecurityConfiguration {
             // forwards to this endpoint to pass the authentication data to DSpace.
             .addFilterBefore(new SamlLoginFilter("/api/authn/saml", HttpMethod.GET.name(),
                                                  authenticationManager, restAuthenticationService),
-                             LogoutFilter.class)
-            // Add a custom Token based authentication filter based on the token previously given to the client
-            // before each URL
-            .addFilterBefore(new StatelessAuthenticationFilter(authenticationManager, restAuthenticationService,
-                                                               ePersonRestAuthenticationProvider, requestService),
-                             StatelessLoginFilter.class);
+                             LogoutFilter.class);
         return http.build();
     }
 
