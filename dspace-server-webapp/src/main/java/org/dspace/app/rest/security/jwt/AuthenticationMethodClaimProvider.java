14cce7930e11e01ea56d67cd7d205df14a9a7ded
tamu-sad-iii <wwelling@tamu.edu>
2025-10-15 15:56:38 -0500

    Add auth method check in authentication method claim provider get value

--- dspace-server-webapp/src/main/java/org/dspace/app/rest/security/jwt/AuthenticationMethodClaimProvider.java
+++ dspace-server-webapp/src/main/java/org/dspace/app/rest/security/jwt/AuthenticationMethodClaimProvider.java
@@ -12,6 +12,8 @@ import java.text.ParseException;
 
 import com.nimbusds.jwt.JWTClaimsSet;
 import jakarta.servlet.http.HttpServletRequest;
+
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.dspace.authenticate.service.AuthenticationService;
@@ -39,10 +41,74 @@ public class AuthenticationMethodClaimProvider implements JWTClaimProvider {
 
     @Override
     public Object getValue(final Context context, final HttpServletRequest request) {
-        if (context.getAuthenticationMethod() != null) {
-            return context.getAuthenticationMethod();
+
+        final String servletPath = request.getServletPath();
+
+        threadRequestSystemOut(context, request, "AMCP: Request servlet path: " + servletPath);
+        String authMethod = null;
+
+        switch (servletPath) {
+            case "/api/authn/login":
+                String user = request.getParameter("user");
+                String password = request.getParameter("password");
+
+                if (StringUtils.isNotEmpty(user) && StringUtils.isNotEmpty(password)) {
+                    authMethod = "password"; // new PasswordAuthentication().getName()
+                    threadRequestSystemOut(context, request, "AMCP: Password Authentication");
+                } else {
+                    authMethod = null;
+                }
+                break;
+            case "/api/authn/shibboleth":
+                authMethod = "shib"; // new ShibAuthentication().getName()
+                threadRequestSystemOut(context, request, "AMCP: Shibboleth Authentication");
+                break;
+            case "/api/authn/orcid":
+                authMethod = "orcid"; // new OrcidAuthentication().getName()
+                threadRequestSystemOut(context, request, "AMCP: Orcid Authentication");
+                break;
+            case "/api/authn/oidc":
+                authMethod = "oidc"; // new OidcAuthentication().getName()
+                threadRequestSystemOut(context, request, "AMCP: OIDC Authentication");
+                break;
+            case "/api/authn/saml":
+                authMethod = "saml"; // new SamlAuthentication().getName()
+                threadRequestSystemOut(context, request, "AMCP: SAML Authentication");
+                break;
+            default:
+                break;
         }
-        return authenticationService.getAuthenticationMethod(context, request);
+
+        if (StringUtils.isNotEmpty(authMethod)) {
+            threadRequestSystemOut(context, request, "AMCP: Setting auth method " + authMethod + " on context from request URL matching login filter");
+            context.setAuthenticationMethod(authMethod);
+        } else {
+            threadRequestSystemOut(context, request, "AMCP: Auth method not known yet. Checking request attribute am");
+            authMethod = (String) request.getAttribute("am");
+            
+            if (StringUtils.isNotEmpty(authMethod)) {
+                threadRequestSystemOut(context, request, "AMCP: Setting auth method " + authMethod + " on context from request attribute am");
+                context.setAuthenticationMethod(authMethod);
+            } else {
+                threadRequestSystemOut(context, request, "AMCP: Request attribute am not defined");
+            }
+        }
+    
+        String lazyAuthMethod = authenticationService.getAuthenticationMethod(context, request);
+
+        if (StringUtils.isNotEmpty(authMethod)) {
+            context.setAuthenticationMethod(lazyAuthMethod);
+        } else {
+            if (lazyAuthMethod.equals(authMethod)) {
+                threadRequestSystemOut(context, request, "AMCP: Authentication service returned same auth method as on the context");
+            } else {
+                threadRequestSystemOut(context, request, "AMCP: Authentication service returned a different auth method as on the context");
+                threadRequestSystemOut(context, request, "AMCP: Authentication service auth method " + lazyAuthMethod);
+                threadRequestSystemOut(context, request, "AMCP: Context auth method " + authMethod);
+            }
+        }
+
+        return context.getAuthenticationMethod();
     }
 
     @Override
@@ -52,8 +118,20 @@ public class AuthenticationMethodClaimProvider implements JWTClaimProvider {
             String authMethod = jwtClaimsSet.getStringClaim(AUTHENTICATION_METHOD);
             context.setAuthenticationMethod(authMethod);
             request.setAttribute("am", authMethod);
+            threadRequestSystemOut(context, request, "AMCP: parsed auth method " + authMethod + " from stateless token");
         } catch (ParseException e) {
             log.error(e::getMessage, e);
         }
+    }
+
+    private void threadRequestSystemOut(Context context, HttpServletRequest request, String message) {
+        System.out.println(
+            String.format(
+                "Context %10s - thread %10s - request %10s: %s",
+                context.hashCode(),
+                Thread.currentThread().getId(),
+                request.getRequestId(), message
+            )
+        );
     }
 }
