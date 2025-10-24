ff6ea94c608341f79707a333fa10edd47094fa9d
tamu-sad-iii <144841721+tamu-sad-iii@users.noreply.github.com>
2025-10-24 07:44:07 -0500

    Null check request

--- dspace-api/src/main/java/org/dspace/authenticate/AuthenticationUtility.java
+++ dspace-api/src/main/java/org/dspace/authenticate/AuthenticationUtility.java
@@ -79,7 +79,7 @@ public class AuthenticationUtility {
     public static void updateAuthenticationMethod(Context context, HttpServletRequest request) {
         String authMethod = context.getAuthenticationMethod();
 
-        if (StringUtils.isBlank(authMethod)) {
+        if (request != null && StringUtils.isBlank(authMethod)) {
             authMethod = Mapping.getMethodName(request.getServletPath());
         }
 
